e7c3c810e9c053b8a879304985b7095dd0f58af6
martin.nemec <nneemmeecc.m@gmail.com>
2024-02-19 13:56:46 +0100

    tcom

--- src/components/nemo260-ambulance-wl-list/nemo260-ambulance-wl-list.tsx
+++ src/components/nemo260-ambulance-wl-list/nemo260-ambulance-wl-list.tsx
@@ -12,7 +12,7 @@ export class Nemo260AmbulanceWlList {
   private async getWaitingPatientsAsync(){
     return await Promise.resolve(
       [{
-          name: 'Jožko Púčik',
+          name: 'Jožko Pouk',
           patientId: '10001',
           since: new Date(Date.now() - 10 * 60).toISOString(),
           estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
